4aee961c8cc5c73b587e921856d1846805b0f97d
Raiden Awkward <raiden.ht@gmail.com>
2011-02-10 01:09:13 +0800

    class LinerListBase finished
    modified: version 1.1.1

--- include/linerlistbase.h
+++ include/linerlistbase.h
@@ -3,11 +3,10 @@
  * discript     : base liner table
  * author       : Raiden
  * date         : 2011-02-09
- * version      : 0.1.0
+ * version      : 1.1.1
  */
 
 #include<stdlib.h>
-#include<unistd.h>
 
 template <class T>
 class LinerListBase
@@ -15,23 +14,45 @@ class LinerListBase
 public:
 	/// construct and deconstruct funs
 	LinerListBase(void);
-	LinerListBase(const LinerListBase<T> &copy);
+	LinerListBase(const LinerListBase<T>& copy);
 	~LinerListBase();
 
-	/// get value
+	/// get charas
 	T* getList();
 	int length();
 	int size();
+	bool empty();
+	bool isNull();
 
 	/// operations
 	void clear();
-	T& getElement(int index);
+	T getElement(int index);
+	T& get(int index);
+	T& at(int index);
+	int locate(const T elem,bool(*compare)(const T elem1,const T elem2));//return the location of elem according to compare
+	T& first();
+	T& last();
+	int firstLocation(const T elem);
+	int lastLocation(const T elem);
+	void set(int index, const T elem);
+	bool insert(int position, const T elem);
+	void append(const T elem);
+	void append(const T* elems, int length);
+	void append(const LinerListBase<T>& list);
+	void remove(int position, int n);
+	void removeAt(int position);
+
 	/// operators
 	LinerListBase& operator = (const LinerListBase& list);
+	T& operator [] (int index);
+	T operator [] (int index) const;
 protected:
+	virtual void traverse(int(*visit)(const T&));
 private:
 	void initList();
+	void reserve(int size);//ensures that size of list is not less than specified value
 private:
+	/// sq list data
 	T* _list;//liner list
 	int _length;//list length
 	int _size;//list size
--- src/linerlistbase.cpp
+++ src/linerlistbase.cpp
@@ -27,7 +27,7 @@ template<class T> LinerListBase<T>::LinerListBase(const LinerListBase<T>& copy)
 
 template<class T> LinerListBase<T>::~LinerListBase()
 {
-
+	this->clear();
 }
 
 template<class T> T* LinerListBase<T>::getList()
@@ -45,6 +45,16 @@ template<class T> int LinerListBase<T>::size()
 	return this->_size;
 }
 
+template<class T> bool LinerListBase<T>::empty()
+{
+	return (_length == 0);
+}
+
+template<class T> bool LinerListBase<T>::isNull()
+{
+	return (_list == NULL);
+}
+
 template<class T> void LinerListBase<T>::clear()
 {
 	if (this->_list)
@@ -57,7 +67,7 @@ template<class T> void LinerListBase<T>::clear()
 }
 
 template<class T>
-T& LinerListBase<T>::getElement(int index)
+T LinerListBase<T>::getElement(int index)
 {
 	if (index < 0 || index >= _length)
 	{
@@ -66,7 +76,143 @@ T& LinerListBase<T>::getElement(int index)
 	return this->_list[index];
 }
 
-template<class T>
+template <class T>
+T& LinerListBase<T>::get(int index)
+{
+	if (index < 0 || index >= _length)
+		return NULL;
+	return this->_list[index];
+}
+
+template <class T>
+T& LinerListBase<T>::at(int index)
+{
+	return this->get(index);
+}
+
+template <class T>
+int LinerListBase<T>::locate(const T elem,bool(*compare)(const T elem1,const T elem2))
+{
+	for (int i = 0; i < _length; ++i)
+	{
+		if ((*compare)(elem,this->getElement(i)))
+			return i;
+	}
+	return 0;
+}
+
+template <class T>
+T& LinerListBase<T>::first()
+{
+	if (_size)
+	{
+		return _list[0];
+	}
+	return NULL;
+}
+
+template <class T>
+T& LinerListBase<T>::last()
+{
+	if (_size)
+	{
+		return _list[size];
+	}
+	return NULL;
+}
+
+template <class T>
+int LinerListBase<T>::firstLocation(const T elem)
+{
+	for (int i = 0; i < _length; ++i)
+	{
+		if (elem == this->getElement(i))
+			return i;
+	}
+	return -1;
+}
+
+template <class T>
+int LinerListBase<T>::lastLocation(const T elem)
+{
+	for (int i = _length -1; i == 0; --i)
+    {
+        if (elem == this->getElement(i))
+            return i;
+    }
+    return -1;
+}
+
+template <class T>
+void LinerListBase<T>::set(int index, const T elem)
+{
+	this->reserve(index);
+	_list[index] = elem;
+}
+
+template <class T>
+bool LinerListBase<T>::insert(int pos, const T item)
+{
+	if (pos < 0)
+	{
+		return false;
+	}
+	if ( _length == 0)
+		reserve(1);
+	if ( pos >= _length )
+		reserve(pos);
+	for (int i=_length; i > pos; --i)
+		_list[i] = _list[i - 1];
+	_list[pos] = item;
+	++_length;
+	return true;
+}
+
+template <class T>
+void LinerListBase<T>::append(const T elem)
+{
+	this->insert(_length,elem);
+}
+
+template <class T>
+void LinerListBase<T>::append(const T* elems, int length)
+{
+	reserve(_length + length);
+	for (int i = 0; i < length; ++i)
+		_list[i + length] = elems[i];
+	_length += length;
+}
+
+template <class T>
+void LinerListBase<T>::append(const LinerListBase<T>& list)
+{
+	reserve(_length + list.length());
+	for(int i = 0; i < list.length(); ++i)
+		_list[_length + i] = list.getElement(i);
+	_length += list.length();
+}
+
+template <class T>
+void LinerListBase<T>::remove(int pos, int n)
+{
+	if ( pos < 0 || n <= 0 || pos + n > _length )
+		return;
+	int i;
+    for (i = pos + n; i < _length; ++i)
+    {
+		_list[i - n] = _list[i];
+	}
+	_length -= n;
+	_size = _length;
+}
+
+template <class T>
+void LinerListBase<T>::removeAt(int pos)
+{
+	remove(pos,1);
+}
+
+template <class T>
 LinerListBase<T>& LinerListBase<T>::operator = (const LinerListBase& list)
 {
 	this->clear();
@@ -88,14 +234,63 @@ LinerListBase<T>& LinerListBase<T>::operator = (const LinerListBase& list)
 }
 
 template <class T>
+T& LinerListBase<T>::operator [] (int index)
+{
+	if (index < 0 || index >= _length)
+		return NULL;
+	return this->_list[index];
+}
+
+template <class T>
+T LinerListBase<T>::operator [] (int index) const
+{
+	if (index < 0 || index >= _length)
+		return NULL;
+	return this->_list[index];
+}
+
+template <class T>
+void LinerListBase<T>::traverse(int(*visit)(const T&))
+{
+	for(int i = 0; i < this->length(); ++i)
+		(*visit)(this->getElement(i));
+}
+
+template <class T>
 void LinerListBase<T>::initList()
 {
 	this->clear();
 }
 
-/// main function for test
-int main(int argc , char** argv)
+template <class T>
+void LinerListBase<T>::reserve(int size)
 {
-	LinerListBase<int>();
+	if ( size > _size )
+	{
+		T* tmp_list = new T[ size ];
+		if ( _list )
+		{
+			for ( int i = 0; i < _length; ++i )
+				tmp_list[i] = _list[i];
+			delete [] _list;
+		}
+		_list = tmp_list;
+		_size = size;
+	}
+}
+
+/// main function for test
+#include<stdio.h>
+int main()
+{
+	LinerListBase<int> list;
+	list.append(1);
+	list.append(2);
+	list.append(3);
+	list.remove(0,3);
+	for (int i = 0; i < list.length(); ++i)
+	{
+		printf("elem : %d is %d\n",i,list.getElement(i));
+	}
 	return 0;
 }
